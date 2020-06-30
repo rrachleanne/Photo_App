8b3084d3cc54f5edf8270c51ccac9fe90762a6b6
Nandini Nayak <nandini.r.nayak@gmail.com>
2020-06-30 16:17:58 +1000

    modal working with js code added and form position updated

--- app/javascript/packs/application.js
+++ app/javascript/packs/application.js
@@ -8,6 +8,15 @@ require("turbolinks").start()
 require("@rails/activestorage").start()
 require("channels")
 
+$('#exampleModal').on('show.bs.modal', function (event) {
+    var button = $(event.relatedTarget) // Button that triggered the modal
+    var recipient = button.data('whatever') // Extract info from data-* attributes
+    // If necessary, you could initiate an AJAX request here (and then do the updating in a callback).
+    // Update the modal's content. We'll use jQuery here, but you could use a data binding library or other methods instead.
+    var modal = $(this)
+    modal.find('.modal-title').text('New message to ' + recipient)
+    modal.find('.modal-body input').val(recipient)
+})
 
 // Uncomment to copy all static images under ../images to the output folder and reference
 // them with the image_pack_tag helper in views (e.g <%= image_pack_tag 'rails.png' %>)
